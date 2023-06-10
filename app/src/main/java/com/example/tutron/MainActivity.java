9b78a15b099ff262e4c182ed93700e15f9115fb5
Yayo <ysale022@uottawa.ca>
2023-06-10 19:38:11 -0400

    Programmed registration (navigation) buttons on MainActivity.

--- app/src/main/java/com/example/tutron/MainActivity.java
+++ app/src/main/java/com/example/tutron/MainActivity.java
@@ -2,13 +2,56 @@ package com.example.tutron;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText editTextEmail, editTextPassword;
+    private Button btnLogin, btnRegisterStudent, btnRegisterTutor;
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Initialize component vars
+        editTextEmail = findViewById(R.id.editTextEmail);
+        editTextPassword = findViewById(R.id.editTextPassword);
+        btnLogin = findViewById(R.id.btnLogin);
+        btnRegisterStudent = findViewById(R.id.btnRegisterStudent);
+        btnRegisterTutor = findViewById(R.id.btnRegisterTutor);
+
+        //Access shared FirebaseAuth instance
+        mAuth = FirebaseAuth.getInstance();
+
+//        btnLogin.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//            }
+//        });
+
+        btnRegisterStudent.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, StudentRegistrationActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        btnRegisterTutor.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, TutorRegistrationActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
+
 }
--- app/src/main/java/com/example/tutron/StudentRegistrationActivity.java
+++ app/src/main/java/com/example/tutron/StudentRegistrationActivity.java
@@ -1,7 +1,6 @@
 package com.example.tutron;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
 public class StudentRegistrationActivity extends AppCompatActivity {
