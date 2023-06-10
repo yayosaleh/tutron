9fe0a2fc79904213d66ef2111837abe417a9bc82
Yayo <ysale022@uottawa.ca>
2023-06-09 21:51:18 -0400

    Created student and tutor registration activity layouts.

---
+++ app/src/main/java/com/example/tutron/TutorRegistrationActivity.java
@@ -0,0 +1,14 @@
+package com.example.tutron;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TutorRegistrationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tutor_registration);
+    }
+}
