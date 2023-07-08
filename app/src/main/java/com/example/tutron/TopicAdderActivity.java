6f0a4eacd8573bb2c86c59d790a7110498ac311c
Yayo <ysale022@uottawa.ca>
2023-07-08 15:35:23 -0400

    Created tutor profile, topic manager, and topic adder activities (all currently empty) to facilitate topic management.

---
+++ app/src/main/java/com/example/tutron/TopicAdderActivity.java
@@ -0,0 +1,14 @@
+package com.example.tutron;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TopicAdderActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_topic_adder);
+    }
+}
